1fc719621a6c7171ff6ab9defdbf19866e6257c4
Fajar <muamarfajar9@gmail.com>
2023-04-02 14:15:40 +0700

    Upload Tugas

---
+++ 21_React Testing/praktikum/React-Testing/src/componen/CreateProduct.test.js
@@ -0,0 +1,25 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import CreateProduct from '@/componen/main';
+
+describe('CreateProduct', () => {
+    test('inputted Product Name', () => {
+        render(CreateProduct);
+        screen.debug();
+        // const productNameInput = screen.getByPlaceholderText('Product Name');
+        // const productName = 'Product';
+        // fireEvent.change(productNameInput, { target: { value: productName } });
+        // expect(productNameInput).toHaveValue(productName);
+        // expect(screen.getByText(productName)).toBeInTheDocument();
+    });
+});
+
+// describe('Form Validation', () => {
+//     test('Product Name not be empty', () => {
+//         const inputData = {
+//             productName: '',
+//             // isian data lainnya
+//         };
+//         const result = validateInput(inputData);
+//         expect(result).toBe(false);
+//     });
+// });
