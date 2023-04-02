a955926b5248d140e2e82b65e5d392d1cfc52f3f
Fajar <muamarfajar9@gmail.com>
2023-04-02 15:07:43 +0700

    Upload Tugas

--- 21_React Testing/praktikum/React-Testing/src/componen/CreateProduct.test.js
+++ 21_React Testing/praktikum/React-Testing/src/componen/CreateProduct.test.js
@@ -5,21 +5,20 @@ describe('CreateProduct', () => {
     test('inputted Product Name', () => {
         render(CreateProduct);
         screen.debug();
-        // const productNameInput = screen.getByPlaceholderText('Product Name');
-        // const productName = 'Product';
-        // fireEvent.change(productNameInput, { target: { value: productName } });
-        // expect(productNameInput).toHaveValue(productName);
-        // expect(screen.getByText(productName)).toBeInTheDocument();
+        const productNameInput = screen.getByPlaceholderText('Product Name');
+        const productName = 'Product';
+        fireEvent.change(productNameInput, { target: { value: productName } });
+        expect(productNameInput).toHaveValue(productName);
+        expect(screen.getByText(productName)).toBeInTheDocument();
     });
 });
 
-// describe('Form Validation', () => {
-//     test('Product Name not be empty', () => {
-//         const inputData = {
-//             productName: '',
-//             // isian data lainnya
-//         };
-//         const result = validateInput(inputData);
-//         expect(result).toBe(false);
-//     });
-// });
+describe('Form Validation', () => {
+    test('Product Name not be empty', () => {
+        const inputData = {
+            productName: ''
+        };
+        const result = validateInput(inputData);
+        expect(result).toBe(false);
+    });
+});
